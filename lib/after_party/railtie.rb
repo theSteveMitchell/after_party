fd3f0b7ba78843ea06d04226bc60171e00d6be44
Steve Mitchell <theSteveMitchell@gmail.com>
2013-02-05 16:25:37 -0600

    use railtie to load data_version files and generators

--- lib/after_party/railtie.rb
+++ lib/after_party/railtie.rb
@@ -1,9 +1,22 @@
 require 'after_party'
+require 'after_party'
 require 'rails'
 module AfterParty
   class Railtie < Rails::Railtie
     rake_tasks do
       load "tasks/deploy_task_runner.rake"
     end
+
+    initializer "load_data_version_models" do
+      load "data_version.rb"
+      load "data_version_file.rb"
+    end
+
+    generators do
+      load "generators/task_generator.rb"
+      load "generators/install_generator.rb"
+    end
   end
+
+
 end
