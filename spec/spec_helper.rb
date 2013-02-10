659dc21ee84c7d93f4ec845580c7b60ba888b34c
Steve Mitchell <theSteveMitchell@gmail.com>
2013-02-10 16:58:46 -0600

    fix spec_helper include paths

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,28 +4,13 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-#Dir[Rails.root.join("lib/*.rb")].each {|f| require f}
 Dir[Rails.root.join("lib/*.rb")].each {|f| require f}
-Dir[Rails.root.join("lib/generators/*.rb")].each {|f| require f}
-
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir[Rails.root.join("lib/generators/install/*.rb")].each {|f| require f}
+Dir[Rails.root.join("lib/generators/task/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
 
   config.include FactoryGirl::Syntax::Methods
-
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
