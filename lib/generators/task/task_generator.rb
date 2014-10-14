d61a03bcbe86a1d96eaa98cf199c2df7cd2b8894
Guillaume Montard <montard@vodeclic.com>
2014-10-14 16:17:34 +0200

    [Debug] You cannot have "name" as required argument after the non-required argument "description". (ArgumentError)

--- lib/generators/task/task_generator.rb
+++ lib/generators/task/task_generator.rb
@@ -1,7 +1,7 @@
 require 'date'
 module AfterParty
   module Generators
-    class TaskGenerator < Rails::Generators::NamedBase
+    class TaskGenerator < Rails::Generators::Base
       source_root File.expand_path('../templates', __FILE__)
       argument :name, :type => :string
       argument :description, :type => :string, :default => ''
