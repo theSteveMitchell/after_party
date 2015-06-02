ad03da1aecd061190427f51bcb6f9f03c7394a10
Steve Mitchell <SMitchell@groupon.com>
2015-06-02 13:57:15 -0500

    fix namespaces

--- lib/after_party/models/mongoid/task_record.rb
+++ lib/after_party/models/mongoid/task_record.rb
@@ -1,13 +1,14 @@
-class TaskRecord
-  include Mongoid::Document
-  include Mongoid::Timestamps
+module AfterParty
+  class TaskRecord
+    include Mongoid::Document
+    include Mongoid::Timestamps
 
-  field :version, type: String
+    field :version, type: String
 
 
-  def self.completed_task?(version)
-    all.any?{|t| t.version == version}
+    def self.completed_task?(version)
+      all.any?{|t| t.version == version}
+    end
   end
-
 end
 
