b2ffe64f23eb4ef8f1d39eff92294022141d4f58
Marcin Wyszynski <marcin.pixie@gmail.com>
2015-08-05 20:03:44 +0200

    Improve templates for Ruby 2.x

--- lib/generators/task/task_generator.rb
+++ lib/generators/task/task_generator.rb
@@ -1,27 +1,38 @@
 require 'date'
+
 module AfterParty
   module Generators
     class TaskGenerator < Rails::Generators::Base
-      source_root File.expand_path('../templates', __FILE__)
-      argument :name, :type => :string
-      class_option :description, :type => :string, :description => "Include a description"
+      source_root(File.expand_path('../templates', __FILE__))
+      argument(:name, type: :string)
+      class_option(
+        :description,
+        type: :string,
+        description: 'Include a description'
+      )
 
       def copy_deploy_task
-        template "deploy.rake",  "lib/tasks/deployment/#{timestamp}_#{file_name}.rake"
+        template(
+          'deploy.txt.erb',
+          "lib/tasks/deployment/#{timestamp}_#{file_name}.rake"
+        )
       end
 
       private
+
       def file_name
         name.underscore
       end
 
       def task_description
-        "Deployment task: #{options.description.blank? ? file_name : options.description}"
+        line = 'Deployment task: '
+        return line + file_name if options.description.blank?
+        line + options.description
       end
 
       def timestamp
-        @timestamp ||= Time.now.utc.strftime("%Y%m%d%H%M%S")
+        @timestamp ||= Time.now.utc.strftime('%Y%m%d%H%M%S')
       end
-    end
-  end
-end
+    end  # class TaskGenerator
+  end  # module Generators
+end  # module AfterParty
