3590f9d36b855f5cb4b5ba0e5a50fd6601974eb4
Steve Mitchell <smitchell@groupon.com>
2019-03-05 10:43:40 -0500

    remove reference to Rails assets, deprecated in V 5

--- config/application.rb
+++ config/application.rb
@@ -46,11 +46,5 @@ module AfterParty
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
     # config.active_record.schema_format = :sql
-
-    # Enable the asset pipeline
-    config.assets.enabled = true
-
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,11 +19,5 @@ AfterParty::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  # Do not compress assets
-  config.assets.compress = false
-
-  # Expands the lines which load the assets
-  config.assets.debug = true
-
   config.eager_load = false
 end
