1bc742b2da93a3f67bfd1ab12d33f027741d2efd
Steve Mitchell <smitchell@groupon.com>
2019-03-05 14:44:56 -0500

    simplify specs and adopt expect syntax finally

---
+++ spec/models/task_recorder_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+require 'fileutils'
+
+module AfterParty
+  describe TaskRecorder do
+    include FileUtils
+
+    before(:all) do
+
+      FILE_PATH = File.join(Rails.root, "spec/fixtures/tasks/deployment/")
+      silence_warnings do
+        TaskRecorder::FILE_MASK = File.join(FILE_PATH, "/*.rake")
+      end
+    end
+
+    describe "pending_files" do
+
+      context "when no tasks have been completed" do
+
+        it "should return an ordered list of all tasks" do
+          list = TaskRecorder.pending_files
+          expect(list.count).to eq 3
+
+          expect(list.first.filename).to match /20120205141454_m_three.rake/
+          expect(list[1].filename).to match /20130205176456_z_first.rake/
+          expect(list[2].filename).to match /20130207948264_a_second2.rake/
+        end
+      end
+
+      context "When some tasks have been completed" do
+
+        before(:all) do
+          a = create :task_record, :version => "20120205141454"
+          b = create :task_record, :version => "20130207948264"
+        end
+
+        after(:all) do
+          TaskRecord.delete_all
+        end
+
+        it "should not include tasks that have already been recorded in the database" do
+          list = TaskRecorder.pending_files
+          expect(list.count).to eq 1
+          expect(list[0].filename).to match /20130205176456_z_first.rake/
+        end
+      end
+    end
+
+
+  end
+end
