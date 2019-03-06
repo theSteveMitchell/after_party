27194f757c5535c56bd82841823cd77dd172dbb2
Steve Mitchell <smitchell@groupon.com>
2019-03-06 08:14:53 -0500

    remove attr_accessor

--- lib/after_party/models/active_record/task_record.rb
+++ lib/after_party/models/active_record/task_record.rb
@@ -1,8 +1,6 @@
 module AfterParty
   class TaskRecord < ActiveRecord::Base
-    if ::Rails::VERSION::MAJOR.to_i == 3
-      attr_accessible :version
-    end
+
     def self.completed_task?(version)
       all.any?{|t| t.version == version}
     end
