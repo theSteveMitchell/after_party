bce1c64c3487ee323dac3db9bd90f9a051eb2659
Nevada James Start <nevadajames@gmail.com>
2019-03-28 17:34:01 +0100

    update test_generator.rb

--- lib/generators/test/test_generator.rb
+++ lib/generators/test/test_generator.rb
@@ -1,1 +1,19 @@
+class TestGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../templates', __FILE__)
 
+  def create_test_file
+    @clean_file_name = file_name.gsub(/\d{14}_{1}/, '')
+    fail file_exist_warning if File.exist?(file_extension)
+    template "test_template.erb", file_extension
+  end
+
+  private
+
+  def file_extension
+    "spec/lib/tasks/deployment/#{@clean_file_name}_spec.rb"
+  end
+
+  def file_exist_warning
+    "A file with name #{file_extension} already exists"
+  end
+end
